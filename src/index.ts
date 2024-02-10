baa8d17d42e9c04e9d541e902861fbe952f6e6be
Anna Perevedentseva <anna.pereved@gmail.com>
2024-02-10 22:17:21 +0100

    feat: create empty server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const server = http.createServer();
+
+server.listen(PORT, () => console.log(`App is listening on url http://localhost:${PORT}`));

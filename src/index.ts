b196e5b4facd0f7260b52668e2f6645472a49036
Anna Perevedentseva <anna.pereved@gmail.com>
2024-02-11 22:08:43 +0100

    feat: implement put method

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,6 @@ const users: User[] = [
     age: 20,
     hobbies: ['programming'],
   },
-  {
-    id: uuidv4(),
-    username: 'Jane Doe',
-    age: 37,
-    hobbies: [],
-  },
 ];
 
 server.listen(PORT, () =>
@@ -32,9 +26,11 @@ server.on('request', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   if (method === 'GET' && url === '/api/users') {
+    // get all users
     res.statusCode = 200;
     res.end(JSON.stringify(users));
   } else if (method === 'GET' && url?.startsWith('/api/users/')) {
+    // get user by id
     const reqUserId = url.slice(11);
     const user = users.find((user) => user.id === reqUserId);
     if (user) {
@@ -50,6 +46,7 @@ server.on('request', (req, res) => {
       }
     }
   } else if (method === 'POST' && url === '/api/users') {
+    // create new user
     let body = '';
     req.on('data', (data) => {
       if (data) {
@@ -73,7 +70,44 @@ server.on('request', (req, res) => {
         res.end('Request should contain correct required fields');
       }
     });
+  } else if (method === 'PUT' && url?.startsWith('/api/users/')) {
+    // update existing user
+    const reqUserId = url.slice(11);
+    const userIdInArray = users.findIndex((user) => user.id === reqUserId);
+    if (userIdInArray >= 0) {
+      let body = '';
+      req.on('data', (data) => {
+        if (data) {
+          body += data.toString();
+        }
+      });
+      req.on('end', () => {
+        const parsedBody: unknown = JSON.parse(body);
+        if (validateUserData(parsedBody)) {
+          users[userIdInArray] = {
+            id: users[userIdInArray].id,
+            username: parsedBody.username,
+            age: parsedBody.age,
+            hobbies: parsedBody.hobbies,
+          };
+          res.statusCode = 200;
+          res.end(JSON.stringify(users[userIdInArray]));
+        } else {
+          res.statusCode = 400;
+          res.end('Request should contain correct required fields');
+        }
+      });
+    } else {
+      if (!uuidValidate(reqUserId)) {
+        res.statusCode = 400;
+        res.end('Invalid user id');
+      } else {
+        res.statusCode = 404;
+        res.end('User not found');
+      }
+    }
   } else if (method === 'DELETE' && url?.startsWith('/api/users/')) {
+    //  delete user
     const reqUserId = url.slice(11);
     const userIdInArray = users.findIndex((user) => user.id === reqUserId);
     if (userIdInArray >= 0) {
@@ -90,6 +124,7 @@ server.on('request', (req, res) => {
       }
     }
   } else {
+    // all other requests
     res.statusCode = 404;
     res.end(`Requested resource doesn't exist`);
   }

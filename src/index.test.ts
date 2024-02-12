442458d2e3552d1eba93aca5eb1022778aa9b298
Anna Perevedentseva <anna.pereved@gmail.com>
2024-02-12 13:14:54 +0100

    feat: implement tests for the second scenario

--- src/index.test.ts
+++ src/index.test.ts
@@ -69,3 +69,60 @@ describe('Scenario 1', () => {
     expect(res.body).toEqual({ message: 'Invalid user id' });
   });
 });
+
+describe('Scenario 2', () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  const newUserInvalidData = {
+    username: 'Ada Lovelace',
+    hobbies: 'programming',
+  };
+
+  test('should return empty array', async () => {
+    // GET api/users
+    const res = await request(server).get('/api/users');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual([]);
+  });
+
+  test('should not create new user record with invalid data', async () => {
+    // POST api/users
+    const res = await request(server)
+      .post('/api/users')
+      .send(newUserInvalidData);
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({
+      message: 'Request should contain correct required fields',
+    });
+  });
+
+  test('should not return user record if id is not uuid', async () => {
+    // GET api/user/test
+    const res = await request(server).get('/api/users/test');
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ message: 'Invalid user id' });
+  });
+
+  test('should handle request to non-existing endpoint', async () => {
+    // GET test
+    const res = await request(server).get(`/test`);
+    expect(res.statusCode).toEqual(404);
+    expect(res.body).toEqual({ message: `Requested resource doesn't exist` });
+  });
+
+  test('should handle put request if id is not uuid', async () => {
+    // PUT api/users/test
+    const res = await request(server).put('/api/users/test');
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ message: 'Invalid user id' });
+  });
+
+  test('should handle delete request if id is not uuid', async () => {
+    // DELETE api/users/test
+    const res = await request(server).delete('/api/users/test');
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ message: 'Invalid user id' });
+  });
+});

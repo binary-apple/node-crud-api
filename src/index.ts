6d116caee9a67fee24afd4e580e576dd22436735
Anna Perevedentseva <anna.pereved@gmail.com>
2024-02-12 17:42:44 +0100

    feat: add handling errors on the server side

--- src/index.ts
+++ src/index.ts
@@ -15,119 +15,164 @@ server.listen(PORT, () =>
 );
 
 server.on('request', (req, res) => {
-  const { method, url } = req;
-  res.setHeader('Content-Type', 'application/json');
+  try {
+    const { method, url } = req;
+    res.setHeader('Content-Type', 'application/json');
 
-  if (method === 'GET' && url === '/api/users') {
-    // get all users
-    res.statusCode = 200;
-    res.end(JSON.stringify(users));
-  } else if (method === 'GET' && url?.startsWith('/api/users/')) {
-    // get user by id
-    const reqUserId = url.slice(11);
-    const user = users.find((user) => user.id === reqUserId);
-    if (user) {
+    if (method === 'GET' && url === '/api/users') {
+      // get all users
       res.statusCode = 200;
-      res.end(JSON.stringify(user));
-    } else {
-      if (!uuidValidate(reqUserId)) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid user id' }));
+      res.end(JSON.stringify(users));
+    } else if (method === 'GET' && url?.startsWith('/api/users/')) {
+      // get user by id
+      const reqUserId = url.slice(11);
+      const user = users.find((user) => user.id === reqUserId);
+      if (user) {
+        res.statusCode = 200;
+        res.end(JSON.stringify(user));
       } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'Invalid user id' }));
+        if (!uuidValidate(reqUserId)) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ message: 'Invalid user id' }));
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Invalid user id' }));
+        }
       }
-    }
-  } else if (method === 'POST' && url === '/api/users') {
-    // create new user
-    let body = '';
-    req.on('data', (data) => {
-      if (data) {
-        body += data.toString();
-      }
-    });
-    req.on('end', () => {
-      const parsedBody: unknown = JSON.parse(body);
-      if (validateUserData(parsedBody)) {
-        const newUser = {
-          id: uuidv4(),
-          username: parsedBody.username,
-          age: parsedBody.age,
-          hobbies: parsedBody.hobbies,
-        };
-        users.push(newUser);
-        res.statusCode = 201;
-        res.end(JSON.stringify(newUser));
-      } else {
-        res.statusCode = 400;
-        res.end(
-          JSON.stringify({
-            message: 'Request should contain correct required fields',
-          }),
-        );
-      }
-    });
-  } else if (method === 'PUT' && url?.startsWith('/api/users/')) {
-    // update existing user
-    const reqUserId = url.slice(11);
-    const userIdInArray = users.findIndex((user) => user.id === reqUserId);
-    if (userIdInArray >= 0) {
+    } else if (method === 'POST' && url === '/api/users') {
+      // create new user
       let body = '';
       req.on('data', (data) => {
-        if (data) {
-          body += data.toString();
-        }
-      });
-      req.on('end', () => {
-        const parsedBody: unknown = JSON.parse(body);
-        if (validateUserData(parsedBody)) {
-          users[userIdInArray] = {
-            id: users[userIdInArray].id,
-            username: parsedBody.username,
-            age: parsedBody.age,
-            hobbies: parsedBody.hobbies,
-          };
-          res.statusCode = 200;
-          res.end(JSON.stringify(users[userIdInArray]));
-        } else {
-          res.statusCode = 400;
+        try {
+          if (data) {
+            body += data.toString();
+          }
+        } catch {
+          res.statusCode = 500;
           res.end(
             JSON.stringify({
-              message: 'Request should contain correct required fields',
+              message: 'Internal server error',
             }),
           );
         }
       });
-    } else {
-      if (!uuidValidate(reqUserId)) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid user id' }));
+      req.on('end', () => {
+        try {
+          const parsedBody: unknown = JSON.parse(body);
+          if (validateUserData(parsedBody)) {
+            const newUser = {
+              id: uuidv4(),
+              username: parsedBody.username,
+              age: parsedBody.age,
+              hobbies: parsedBody.hobbies,
+            };
+            users.push(newUser);
+            res.statusCode = 201;
+            res.end(JSON.stringify(newUser));
+          } else {
+            res.statusCode = 400;
+            res.end(
+              JSON.stringify({
+                message: 'Request should contain correct required fields',
+              }),
+            );
+          }
+        } catch (err) {
+          res.statusCode = 500;
+          res.end(
+            JSON.stringify({
+              message: 'Internal server error',
+            }),
+          );
+        }
+      });
+    } else if (method === 'PUT' && url?.startsWith('/api/users/')) {
+      // update existing user
+      const reqUserId = url.slice(11);
+      const userIdInArray = users.findIndex((user) => user.id === reqUserId);
+      if (userIdInArray >= 0) {
+        let body = '';
+        req.on('data', (data) => {
+          try {
+            if (data) {
+              body += data.toString();
+            }
+          } catch {
+            res.statusCode = 500;
+            res.end(
+              JSON.stringify({
+                message: 'Internal server error',
+              }),
+            );
+          }
+        });
+        req.on('end', () => {
+          try {
+            const parsedBody: unknown = JSON.parse(body);
+            if (validateUserData(parsedBody)) {
+              users[userIdInArray] = {
+                id: users[userIdInArray].id,
+                username: parsedBody.username,
+                age: parsedBody.age,
+                hobbies: parsedBody.hobbies,
+              };
+              res.statusCode = 200;
+              res.end(JSON.stringify(users[userIdInArray]));
+            } else {
+              res.statusCode = 400;
+              res.end(
+                JSON.stringify({
+                  message: 'Request should contain correct required fields',
+                }),
+              );
+            }
+          } catch {
+            res.statusCode = 500;
+            res.end(
+              JSON.stringify({
+                message: 'Internal server error',
+              }),
+            );
+          }
+        });
       } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'User not found' }));
+        if (!uuidValidate(reqUserId)) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ message: 'Invalid user id' }));
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'User not found' }));
+        }
       }
-    }
-  } else if (method === 'DELETE' && url?.startsWith('/api/users/')) {
-    //  delete user
-    const reqUserId = url.slice(11);
-    const userIdInArray = users.findIndex((user) => user.id === reqUserId);
-    if (userIdInArray >= 0) {
-      res.statusCode = 204;
-      users.splice(userIdInArray, 1);
-      res.end();
-    } else {
-      if (!uuidValidate(reqUserId)) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Invalid user id' }));
+    } else if (method === 'DELETE' && url?.startsWith('/api/users/')) {
+      //  delete user
+      const reqUserId = url.slice(11);
+      const userIdInArray = users.findIndex((user) => user.id === reqUserId);
+      if (userIdInArray >= 0) {
+        res.statusCode = 204;
+        users.splice(userIdInArray, 1);
+        res.end();
       } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'User not found' }));
+        if (!uuidValidate(reqUserId)) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ message: 'Invalid user id' }));
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'User not found' }));
+        }
       }
+    } else {
+      // all other requests
+      res.statusCode = 404;
+      res.end(JSON.stringify({ message: `Requested resource doesn't exist` }));
     }
-  } else {
-    // all other requests
-    res.statusCode = 404;
-    res.end(JSON.stringify({ message: `Requested resource doesn't exist` }));
+  } catch {
+    res.statusCode = 500;
+    res.end(
+      JSON.stringify({
+        message: 'Internal server error',
+      }),
+    );
   }
 });
 
